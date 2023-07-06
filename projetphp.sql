39f206bc8eef60a6dbad473474cd860aa7daf985
Olivier PERDRIX <operdrix@myges.fr>
2023-07-06 23:28:35 +0200

    Nouvelle extraction sql

--- projetphp.sql
+++ projetphp.sql
@@ -3,25 +3,26 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : ven. 02 juin 2023 à 16:14
+-- Généré le : jeu. 06 juil. 2023 à 21:27
 -- Version du serveur : 8.0.33
--- Version de PHP : 8.1.19
+-- Version de PHP : 8.1.20
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+02:00";
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
-;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
-;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
-;
-/*!40101 SET NAMES utf8mb4 */
-;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Base de données : `projetphp`
 --
 
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `avis`
 --
@@ -29,26 +30,46 @@ SET time_zone = "+02:00";
 CREATE TABLE `avis` (
   `id` int NOT NULL,
   `id_restaurant` int NOT NULL,
-  `avis` text COLLATE utf8mb4_general_ci NOT NULL,
-  `pseudo` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `avis` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pseudo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `note` int NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `avis`
+--
+
+INSERT INTO `avis` (`id`, `id_restaurant`, `avis`, `pseudo`, `note`, `date`) VALUES
+(1, 1, 'blablabla', 'oliv', 5, '2023-07-06 18:14:28'),
+(2, 1, 'iufhpi jhdpiuhfp mdkf', 'oliv', 4, '2023-07-06 18:19:44'),
+(3, 1, 'miojhdsm ijhdpiuhqs dpi', 'pasmoi', 1, '2023-07-06 18:19:44');
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `restaurant`
 --
 
 CREATE TABLE `restaurant` (
   `id` int NOT NULL,
-  `nom` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` text COLLATE utf8mb4_general_ci NOT NULL,
-  `adresse` text COLLATE utf8mb4_general_ci NOT NULL,
-  `telephone` varchar(14) COLLATE utf8mb4_general_ci NOT NULL,
-  `url_photo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `nom_proprietaire` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `nom` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `adresse` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `telephone` varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `url_photo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nom_proprietaire` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `restaurant`
+--
+
+INSERT INTO `restaurant` (`id`, `nom`, `description`, `adresse`, `telephone`, `url_photo`, `email`, `nom_proprietaire`) VALUES
+(1, 'A la bonne franquette', 'Un restau où l\'on mange bien !', 'ssd', '06 03 48 78 56', 'https://images.ctfassets.net/8dreszsahte7/6wpEs9IEZOajWz3kgE6rgj/2b7cf5bd7c76c6aa67088262e9167b0f/restaurant-en-difficulte.jpeg', 'olivierperdrix@gmail.com', 'Olivier PERDRIX'),
+(2, 'Mon restau', 'Ma description', '5 rue de la Tase', '0606587856', 'https://toohotel.com/wp-content/uploads/2022/09/TOO_restaurant_Panoramique_vue_Paris_Seine_Tour_Eiffel_2.jpg', 'olivierperdrix@edf.fr', 'Olivier Perdrix');
+
 --
 -- Index pour les tables déchargées
 --
@@ -57,13 +78,15 @@ CREATE TABLE `restaurant` (
 -- Index pour la table `avis`
 --
 ALTER TABLE `avis`
-ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `id_restaurant` (`id_restaurant`);
+
 --
 -- Index pour la table `restaurant`
 --
 ALTER TABLE `restaurant`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
@@ -72,12 +95,14 @@ ADD PRIMARY KEY (`id`);
 -- AUTO_INCREMENT pour la table `avis`
 --
 ALTER TABLE `avis`
-MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT pour la table `restaurant`
 --
 ALTER TABLE `restaurant`
-MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- Contraintes pour les tables déchargées
 --
@@ -86,11 +111,9 @@ MODIFY `id` int NOT NULL AUTO_INCREMENT;
 -- Contraintes pour la table `avis`
 --
 ALTER TABLE `avis`
-ADD CONSTRAINT `id_restaurant` FOREIGN KEY (`id_restaurant`) REFERENCES `restaurant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `id_restaurant` FOREIGN KEY (`id_restaurant`) REFERENCES `restaurant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
